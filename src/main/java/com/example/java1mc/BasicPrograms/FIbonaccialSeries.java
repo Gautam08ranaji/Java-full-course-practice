5ad745cd2de84b5daa10b31ea2e5224838a6517e
GAUTAM RANA <gautamrana5968@gmail.com>
2024-01-12 15:53:06 +0530

    Create FIbonaccialSeries.java

---
+++ src/main/java/com/example/java1mc/BasicPrograms/FIbonaccialSeries.java
@@ -0,0 +1,16 @@
+package com.example.java1mc.BasicPrograms;
+
+public class FIbonaccialSeries {
+    public static void main(String[] args) {
+        int x = 24;
+        double y = x;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        char a = '@';
+        int b = a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
