7da4f74e3f7f96ec59b13e9f9fa42a5321079057
GAUTAM RANA <gautamrana5968@gmail.com>
2024-03-19 23:18:30 +0530

    ADDED

--- src/main/java/OOPS/ClassAndObjects/Car.java
+++ src/main/java/OOPS/ClassAndObjects/Car.java
@@ -1,36 +1,25 @@
 package OOPS.ClassAndObjects;
 
 public class Car {
-    String car_name,car_type,engine_type;
-    double car_price;
-    public void start(){
-        System.out.println("Started");
-    }
-    public void running(){
-        System.out.println("car is running");
-    }
-    public void brake(){
-        System.out.println("applying brakes");
-    }
-    public void stop(){
-        System.out.println("car  stopped");
-    }
-    public static void main(String[] args) {
-        Car c1 = new Car();
+
+        String car_name,car_type;
+        double price;
+        Engine e1 = new Engine(60,6,"PETROL");
+
+        Car(){
+
+        }
+        Car(String car_name,String car_type,double price){
+            this.car_name=car_name;
+            this.car_type=car_type;
+            this.price=price;
+        }
+        public void displayCar(){
+            System.out.println("CAR NAME IS "+car_name);
+            System.out.println("CAR TYPE IS "+car_type);
+            System.out.println("CAR PRICE IS "+price+" \u20B9");
+            e1.displayEngine();
+        }
 
 
-        System.out.println(c1.car_name = "SCORPIO");
-        System.out.println(c1.car_type = "SUV");
-        System.out.println(c1.engine_type = "DIESEL");
-        System.out.println(c1.car_price = 3000000d);
-        c1.start();
-        c1.running();
-        c1.brake();
-        c1.stop();
-
-
-
-
-
-    }
 }
--- src/main/java/com/example/java1mc/PatternPrinting/PatternPactice/RightPyramid.java
+++ src/main/java/com/example/java1mc/PatternPrinting/PatternPactice/RightPyramid.java
@@ -12,7 +12,7 @@ public class RightPyramid {
 
         for (int i = 1; i <=n ; i++) {
             for (int j = 1; j <= star; j++) {
-                System.out.print(" ");
+                System.out.print("*");
             }
             if (i<(n/2)+1) {
                 star++;
